36492fd83aee50a85d30cf4fb1e0d43f0b69190a
Bijoy <bijoy.ahmed12555@gmail.com>
2024-01-05 01:28:46 +0600

    tutorial commit

--- lib/dasboard_screens/tutorials_screen.dart
+++ lib/dasboard_screens/tutorials_screen.dart
@@ -1,13 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-import '../reusable_widgets/custom_scaffold2.dart';
-class TutorialsScreen extends StatelessWidget {
-  const TutorialsScreen({super.key});
+class TutorialsScreen extends StatefulWidget {
+  const TutorialsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TutorialsScreen> createState() => _YoutubePlayerExampleState();
+}
+
+class _YoutubePlayerExampleState extends State<TutorialsScreen> {
+  final videoURL = "https://youtu.be/PfiQb8pca10?si=8vzq5MyT8uPt6nt1";
+  final videoURL2 = "https://youtu.be/9WLjJWtA27s?si=nvGgMNA50Isu5d8R";
+
+  late YoutubePlayerController _controller;
+  late YoutubePlayerController _controller2;
+
+  @override
+  void initState() {
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+    final videoID2 = YoutubePlayer.convertUrlToId(videoURL2);
+
+    _controller = YoutubePlayerController(
+      initialVideoId: videoID!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+      ),
+    );
+
+    _controller2 = YoutubePlayerController(
+      initialVideoId: videoID2!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+      ),
+    );
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold2(
-      child: Center(child: Text('TUTORIALS',style: TextStyle(color: Colors.amber,fontSize: 60))),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              bottomActions: [
+                CurrentPosition(),
+                ProgressBar(
+                  isExpanded: true,
+                  colors: const ProgressBarColors(
+                    playedColor: Colors.white,
+                    handleColor: Colors.amberAccent,
+                  ),
+                ),
+                FullScreenButton(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: YoutubePlayer(
+              controller: _controller2,
+              showVideoProgressIndicator: true,
+              bottomActions: [
+                CurrentPosition(),
+                ProgressBar(
+                  isExpanded: true,
+                  colors: const ProgressBarColors(
+                    playedColor: Colors.white,
+                    handleColor: Colors.amberAccent,
+                  ),
+                ),
+                FullScreenButton(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
